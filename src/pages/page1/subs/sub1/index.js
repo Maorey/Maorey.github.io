17620494a254de4e8b01ce6285ef38d91620cc29
Maorey <maorey@foxmail.com>
2018-02-24 16:02:41 +0800

    测试获取博客

--- src/pages/page1/subs/sub1/index.js
+++ src/pages/page1/subs/sub1/index.js
@@ -1,12 +1,30 @@
 import React from 'react'
-
 import {Button} from 'antd'
+import axios from 'axios'
 
 export default class Sub1 extends React.Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ''
+    };
+  }
+  componentDidMount() {
+    axios.get('blogs/快速入门MapboxGL.md')
+      .then( (response) => {console.log(response);
+        this.setState({
+          data:response.data
+        });
+      })
+      .catch( (error) => {
+        console.log(error);
+      });
+  }
+  render() {
     return (
       <div >
         <Button>Sub1</Button>
+        <textarea value={this.state.data}></textarea>
       </div>
     )
   }
