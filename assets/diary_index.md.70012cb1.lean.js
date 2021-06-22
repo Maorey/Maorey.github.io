6c3b907f519bc6be1ef45cb35d9cafd8d776fdd3
毛瑞 (via Travis CI) <Maorey@Foxmail.com>
2021-06-22 17:09:15 +0000

    Deploy Maorey/Blog to github.com/Maorey/Maorey.github.io.git:deploy

---
+++ assets/diary_index.md.70012cb1.lean.js
@@ -0,0 +1,1 @@
+import{r as t,o as e,c as a,a as r}from"./app.f71ae3c3.js";const n='{"title":"日记","description":"","frontmatter":{"title":"日记","index":9},"relativePath":"diary/index.md","lastUpdated":1624381669535}',i={},d=r("p",null,"日记索引",-1);i.render=function(n,i,o,l,s,p){const c=t("Menu");return e(),a("div",null,[d,r(c)])};export default i;export{n as __pageData};
